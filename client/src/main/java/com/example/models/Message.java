18451aa69ce3ac94778cb6b6e5fb0e18f30ec286
Diego Saavedra <dsaavedra88@gmail.com>
2024-06-18 09:18:19 -0500

    first commit

---
+++ client/src/main/java/com/example/models/Message.java
@@ -0,0 +1,21 @@
+package com.example.models;
+
+public class Message {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Message() {
+    }
+
+    public Message(String text) {
+        this.text = text;
+    }
+}
